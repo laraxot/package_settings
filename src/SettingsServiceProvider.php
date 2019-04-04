6fb5cb7e490b33ea6b39d9459ba0f50e97d99734
marco xot <marco.sottana@gmail.com>
2019-04-04 19:25:44 +0200

    baseServiceProvider

--- src/SettingsServiceProvider.php
+++ src/SettingsServiceProvider.php
@@ -1,17 +1,14 @@
 <?php
-
-
-
 namespace XRA\Settings;
+use XRA\Extend\BaseServiceProvider;
+//use Illuminate\Support\ServiceProvider;
+//use XRA\Extend\Traits\ServiceProviderTrait;
 
-use Illuminate\Support\ServiceProvider;
-use XRA\Extend\Traits\ServiceProviderTrait;
-
-class SettingsServiceProvider extends ServiceProvider
+class SettingsServiceProvider extends BaseServiceProvider
 {
-    use ServiceProviderTrait;
+    //use ServiceProviderTrait;
 
-    protected $defer = true;
+    //protected $defer = true;
 
     public static function topToolbar()
     {
